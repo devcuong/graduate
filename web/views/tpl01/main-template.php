6d32729d46542bbd2b962f78fe39dceda2ade721
devcuong <cuong.dh8c@gmail.com>
2020-11-20 11:16:38 +0700

    ok

--- web/views/tpl01/main-template.php
+++ web/views/tpl01/main-template.php
@@ -5,13 +5,28 @@
 <title>Trang review đánh giá của khách hàng về các sản phẩm,
 	dịch vụ tại các công ty tại Việt Nam</title>
 <link href="css/bulma.min.css" rel="stylesheet">
-<style type="text/css"></style>
+<style type="text/css">
+   #header-top{
+   	 background-color: #016938;
+   }
+</style>
     </head>
     <body>
 <?php
     echo "this is template 1";
     require_once "./web/views/" . $data["Page"] . ".php"
 ?>
+<section id="header">
+	<div id="header-top">
+		<div class="container d-flex">abc</div>
+	</div>
+</section>
+<section id="main">
+
+</section>
+<section id="footer">
+	
+</section>
 </body>
 </html>
 
